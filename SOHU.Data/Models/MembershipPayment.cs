cab68817d23e90dde16123dbfec61b1cb7986b0d
lethanhson123 <lethanhson123@gmail.com>
2020-07-29 08:58:53 +0700

    appsetting

--- SOHU.Data/Models/MembershipPayment.cs
+++ SOHU.Data/Models/MembershipPayment.cs
@@ -4,8 +4,7 @@ using System.Collections.Generic;
 namespace SOHU.Data.Models
 {
     public partial class MembershipPayment : BaseModel
-    {
-       
+    {       
         public int? MembershipId { get; set; }
         public int? BankId { get; set; }
         public string AccountNumber { get; set; }
